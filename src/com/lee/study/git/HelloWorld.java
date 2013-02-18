b968df793149bbcfca444c9a8bc3302bda7e7536
lixuwei <lixuweiok@gmail.com>
2013-02-18 16:04:24 +0800

    five commit

---
+++ src/com/lee/study/git/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.lee.study.git;
+/**
+ * 第一次使用Egit push to github
+ * @author Alex
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
